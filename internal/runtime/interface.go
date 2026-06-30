6dac64411455b6e3e6334eb6d389898f64569331
clofour <208706398+clofour@users.noreply.github.com>
2026-06-30 22:45:54 +0200

    runtime: implement interface

---
+++ internal/runtime/interface.go
@@ -0,0 +1,32 @@
+package runtime
+
+import "context"
+
+type ContainerStatus string
+
+const (
+	StatusCreated ContainerStatus = "created"
+	StatusRunning ContainerStatus = "running"
+	StatusStopped ContainerStatus = "stopped"
+	StatusUnknown ContainerStatus = "unknown"
+)
+
+type CreateOptions struct {
+	ID    string
+	Image string
+	Env   []string
+}
+
+type ContainerInfo struct {
+	ID     string
+	Status ContainerStatus
+}
+
+type ContainerRuntime interface {
+	Pull(ctx context.Context, image string) error
+	Create(ctx context.Context, options CreateOptions) (string, error)
+	Start(ctx context.Context, containerId string) error
+	Stop(ctx context.Context, containerId string) error
+	Remove(ctx context.Context, containerID string)
+	Inspect(ctx context.Context, containerID string) (ContainerInfo, error)
+}
