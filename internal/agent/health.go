d9b0cdce3255f2eea00d8f7181dfeae3d78c6b70
clofour <208706398+clofour@users.noreply.github.com>
2026-07-01 13:58:20 +0200

    Implement TCP and HTTP healthcheck methods

---
+++ internal/agent/health.go
@@ -0,0 +1,47 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type HealthManager struct {
+	name string
+}
+
+const checkTimeout = 10
+
+func CheckHTTP(ctx context.Context, addr string, port int, path string) (bool, error) {
+	client := &http.Client{}
+	url := fmt.Sprintf("http://%s:%d%s", addr, port, path)
+
+	request, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return false, fmt.Errorf("constructing request %s: %w", url, err)
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return false, fmt.Errorf("doing request %s: %w", url, err)
+	}
+	defer response.Body.Close()
+
+	return response.StatusCode >= 200 && response.StatusCode < 300, nil
+}
+
+func CheckTCP(ctx context.Context, addr string) (bool, error) {
+	dialer := net.Dialer{}
+	conn, err := dialer.DialContext(ctx, "tcp", addr)
+	if err != nil {
+		return false, nil
+	}
+	defer conn.Close()
+
+	return true, nil
+}
+
+func CheckScript() {
+
+}
